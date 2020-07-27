00ee51280783f4c6e826fec29223f686735416ff
Laura Berge <lauraeberge@gmail.com>
2020-07-27 07:49:10 -0500

    adds basic empty board to print to cli

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,7 +3,17 @@
  */
 public class TicTacToe {
 
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args)
+    {
+        printBoard();
+    }
+
+    public static void printBoard()
+    {
+        System.out.println(" | | ");
+        System.out.println("-+-+-");
+        System.out.println(" | | ");
+        System.out.println("-+-+-");
+        System.out.println(" | | ");
     }
 }
