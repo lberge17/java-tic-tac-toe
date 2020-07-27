7fc12931fedb99b82ee0b153253caec4743462ab
Laura Berge <lauraeberge@gmail.com>
2020-07-27 08:01:02 -0500

    adds board variable so that x's ad o's can be added

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,15 +5,25 @@ public class TicTacToe {
 
     public static void main(String[] args)
     {
-        printBoard();
+        char[][] board = {
+            {
+                ' ', ' ', ' '
+            }, {
+                ' ', ' ', ' '
+            }, {
+                ' ', ' ', ' '
+            }
+        };
+
+        printBoard(board);
     }
 
-    public static void printBoard()
+    public static void printBoard(char[][] board)
     {
-        System.out.println(" | | ");
+        System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("-+-+-");
-        System.out.println(" | | ");
+        System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
         System.out.println("-+-+-");
-        System.out.println(" | | ");
+        System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
     }
 }
