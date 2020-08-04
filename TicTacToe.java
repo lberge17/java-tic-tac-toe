972d0cfb36a7ca127ffafadc72a576b665ec2b56
Laura Berge <lauraeberge@gmail.com>
2020-08-03 20:40:57 -0500

    now game properly checks to see if the game is over and acts accordingly

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,10 +2,16 @@
  * TicTacToe
  */
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
+
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
     public static void main(String[] args)
     {
         char[][] board = {
@@ -31,8 +37,16 @@ public class TicTacToe {
             int move = myObj.nextInt();
             if (makeMove(move, board, 'x'))
             {
+                playerPositions.add(move);
                 printBoard(board);
-                cpuMove(board);
+                if (gameOver(board))
+                {
+                    break;
+                }
+                else
+                {
+                    cpuMove(board);
+                }
             }
             else 
             {
@@ -161,7 +175,11 @@ public class TicTacToe {
     public static void cpuMove(char[][] board)
     {
         Random n = new Random();
-        if (!makeMove(n.nextInt(9) + 1, board, 'o'))
+        int move = n.nextInt(9) + 1;
+        if (makeMove(move, board, 'o')) {
+            cpuPositions.add(move);
+        }
+        else
         {
             cpuMove(board);
         }
@@ -169,6 +187,49 @@ public class TicTacToe {
 
     public static boolean gameOver(char[][] board)
     {
+        List<Integer> top = Arrays.asList(1, 2, 3);
+        List<Integer> middleRow = Arrays.asList(4, 5, 6);
+        List<Integer> bottom = Arrays.asList(7, 8, 9);
+        List<Integer> left = Arrays.asList(1, 4, 7);
+        List<Integer> middleCol = Arrays.asList(2, 5, 8);
+        List<Integer> right = Arrays.asList(3, 6, 9);
+        List<Integer> diag1 = Arrays.asList(1, 5, 9);
+        List<Integer> diag2 = Arrays.asList(3, 5, 7);
+
+        List<List<Integer>> winningPositions = new ArrayList<List<Integer>>();
+
+        winningPositions.add(top);
+        winningPositions.add(middleCol);
+        winningPositions.add(bottom);
+        winningPositions.add(left);
+        winningPositions.add(middleRow);
+        winningPositions.add(right);
+        winningPositions.add(diag1);
+        winningPositions.add(diag2);
+
+        for(List<Integer> lis : winningPositions)
+        {
+            if(playerPositions.containsAll(lis))
+            {
+                System.out.println("Congrats! You win!");
+                return true;
+            }
+            else if(cpuPositions.containsAll(lis))
+            {
+                System.out.println("Game over. You lose. :(");
+                return true;
+            }
+            else if (playerPositions.size() + cpuPositions.size() == 9)
+            {
+                System.out.println("Cat's game!");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         return false;
     }
 }
