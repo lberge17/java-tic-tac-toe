d8dc7ee7154da9416c872dfde1a2e96f9a5d5dfb
Laura Berge <lauraeberge@gmail.com>
2020-08-03 20:47:26 -0500

    removes random import since it's no longer used

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,7 +5,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -181,7 +180,7 @@ public class TicTacToe {
                 cpuPositions.add(move);
                 break;
             }
-        } 
+        }
     }
 
     public static boolean gameOver(char[][] board)
